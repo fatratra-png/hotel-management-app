e023165d692cfab0d2a1e75c0a35c696740e56b3
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-06-16 16:40:16 +0300

    fity: Mamorona kilasy Mpiasa

---
+++ src/main/java/origa/hotely/Mpiandry.java
@@ -0,0 +1,7 @@
+package origa.hotely;
+
+public class Mpiandry extends Mpiasa{
+    public Mpiandry(String anarana, String fanampiny, String toeranaNahaterahana, String laharanTarobia) {
+        super(anarana, fanampiny, toeranaNahaterahana, laharanTarobia);
+    }
+}
---
+++ src/main/java/origa/hotely/Mpiasa.java
@@ -0,0 +1,13 @@
+package origa.hotely;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public abstract class Mpiasa {
+    private String anarana;
+    private String fanampiny;
+    private String toeranaNahaterahana;
+    private String laharanTarobia;
+}
