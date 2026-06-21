64e7de335fd43042fc51983aafdf8ad87219191d
fatratra-png <nyfatratrar@gmail.com>
2026-06-21 09:33:48 +0300

    chore:nuked the unwanted mg files pushed

--- src/main/java/origa/hotely/Mpiandry.java
+++
@@ -1,7 +0,0 @@
-package origa.hotely;
-
-public class Mpiandry extends Mpiasa{
-    public Mpiandry(String anarana, String fanampiny, String toeranaNahaterahana, String laharanTarobia) {
-        super(anarana, fanampiny, toeranaNahaterahana, laharanTarobia);
-    }
-}
